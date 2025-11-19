e3c1daf96e68f8bd05a44c2fe83988b872bc03c8
gavinsimpson <gdsimpson3@gmail.com>
2025-11-19 15:05:24 +0000

    Welcome message done

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 using namespace std;
 
-// #include "math.hpp"
 #include "startup.hpp"
 
 int main() {
-    cout << welcomeMessage(2, 3) << "\n";
+    
+    welcomeMessage();
+
+    return 0;
 }
--- src/startup/startup.cpp
+++ src/startup/startup.cpp
@@ -1,5 +1,29 @@
-#include "startup.hpp"
 
-int welcomeMessage(int a, int b) {
-    return a + b;
+#include <iostream>
+
+#include "startup.hpp"
+using namespace std;
+
+void welcomeMessage() {
+    cout << R"(
+    Welcome to NRiP-V1
+    
+    This tool is an implementation of the Newton Raphson Method for Finding roots VIA Iteration formulae
+
+    There are some exceptions as per the Newton Raphson:
+    - It will diverge if you try a point where f(x) has a shallow gradient
+    - The point musnt go to a stationary point
+    - NO complex roots please
+
+    Runtime: C++ (Cmake VERSION 3.22)
+
+    NOTE: ONLY FOR POLYNOMIALS, NOT OTHER TYPES OF FUNCTIONS AS I HAVENT MADE THE DERRIVATIVE CALCULATOR READY FOR THAT
+
+    Write polynomials in this form:
+
+        1X^3 - 3X + 5
+        coefficientX^exponent
+
+    And with That, A теперь наслаждайтесь!
+)"<< std::endl;
 }
