c06166d57243e23a7a5891aff2b4548db74d1200
gavinsimpson <gdsimpson3@gmail.com>
2025-11-19 14:56:07 +0000

    removed build

--- build/src/startup/CMakeFiles/startup.dir/startup.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/startup/CMakeFiles/startup.dir/startup.cpp.o: \
-  /Users/gavinsimpson/Documents/dev/cmake-example-main/src/startup/startup.cpp \
-  /Users/gavinsimpson/Documents/dev/cmake-example-main/src/startup/startup.hpp
